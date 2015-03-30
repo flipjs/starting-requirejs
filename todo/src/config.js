26112fb9e34954e3818e8fd471a7ee4c3f5a81a2
Felipe Apostol <flipjs.io@gmail.com>
2015-03-30 21:41:31 +0100

    initial commit

---
+++ todo/src/config.js
@@ -0,0 +1,19 @@
+require.config({
+    baseUrl: 'src',
+    deps: ['main'],
+    enforceDefine: true,
+    paths: {
+        'knockout': '../external/knockout-3.2.0',
+        'smokesignals': '../external/smokesignals',
+        'text': '../external/text',
+        'views': '../views',
+        'i18n': '../external/i18n'
+    },
+    shim: {
+        'smokesignals': {
+            exports: 'smokesignals'
+        }
+    }
+})
+
+define()
---
+++ todo/src/nls/root/ui_strings.js
@@ -0,0 +1,10 @@
+define({
+    welcome: 'Welcome to the To Do App',
+    createText: 'Create list',
+    placeholderText: 'List name',
+    noListsText: 'No saved lists',
+    loadText: 'Load list',
+    createTaskPlaceholderText: 'Task name',
+    addText: 'Add task',
+    saveText: 'Save list'
+})
